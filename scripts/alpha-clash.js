a92d1975730c35a57daeb7bf62e853aafd534c55
atikmahmud482 <atikmahmud482@gmail.com>
2024-12-23 19:34:20 +0600

    module 27-6

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,14 @@
+// function play() {
+//     // console.log('Playing Alpha Clash');
+//     // step 1: hide the home page. to hide the screen add the class 'hidden' to the element.
+//     document.getElementById('home-screen').classList.add('hidden');
+//     // step 2: show the game screen. to show the screen remove the class 'hidden' from the element.
+//     document.getElementById('play-ground').classList.remove('hidden');
+
+// }
+
+
 function play() {
-    console.log('Playing Alpha Clash');
+    hideElement('home-screen');
+    showElement('play-ground');
 }
